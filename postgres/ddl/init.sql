b8b797e41a33be0e3386a33ec6621ec4cb61eee8
GSergey <g.sergey@bk.ru>
2023-07-02 14:07:25 +0300

    fix: соединение Postgres с Clickhouse

--- postgres/ddl/init.sql
+++ postgres/ddl/init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS dwh
 (
-user_lk_id INTEGER,
+user_lk_id INTEGER PRIMARY KEY,
 user_sex VARCHAR(8),
-user_bod_category SMALLINT DEFAULT NULL,
-user_email VARCHAR(128) DEFAULT NULL,
-user_phone VARCHAR(16) DEFAULT NULL,
-user_most_purshased_model INTEGER DEFAULT NULL,
-user_most_purshased_brend INTEGER DEFAULT NULL,
-user_most_purshased_category INTEGER DEFAULT NULL,
-promo_item_spend_per_month INTEGER DEFAULT NULL
+user_bod_category INTEGER,
+user_email VARCHAR(128),
+user_phone VARCHAR(16),
+user_most_purshased_model INTEGER,
+user_most_purshased_brend INTEGER,
+user_most_purshased_category INTEGER,
+promo_item_spend_per_month INTEGER
 );
